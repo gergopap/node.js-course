6745997ac836c470bd66146a2019f7a9e85c4025
Pap Gergely <pap133@gmail.com>
2019-07-10 13:28:57 +0200

    fix delete, update, connect

--- mongoDB/connect.js
+++ mongoDB/connect.js
@@ -1,8 +1,8 @@
 var MongoClient = require('mongodb').MongoClient;
 var url = 'mongodb://localhost:27017/EmployeeDB';
 
-MongoClient.connect(url, function (err, db) {
-
+MongoClient.connect(url, function (err, client) {
+  var db = client.db('EmployeeDB');
   console.log('connected');
   db.close();
 });
